c3fc0bb2ae4f66ffd0ad089a9e2d0a614a0d57f3
Manoel Guilherme <fisicorj@gmail.com>
2025-05-17 19:14:15 -0300

    up29

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,7 +1,7 @@
 
 from flask import Blueprint, render_template, redirect, url_for, flash, request
 from flask_login import login_user, logout_user, login_required
-from app.models import Usuario
+from app.models import user
 from app.forms import LoginForm
 from app.extensions import db
 
@@ -11,7 +11,7 @@ auth_bp = Blueprint('auth_bp', __name__, url_prefix='/')
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        user = Usuario.query.filter_by(username=form.username.data).first()
+        user = user.query.filter_by(username=form.username.data).first()
         if user and user.check_password(form.password.data):
             login_user(user)
             return redirect(url_for('dashboard.index'))
