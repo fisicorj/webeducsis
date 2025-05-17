4fe21b5778f95c6dea7f2ff20b01f1ca21dc4d84
Manoel Guilherme <fisicorj@gmail.com>
2025-05-17 16:56:58 -0300

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,18 @@
-from app import create_app
+from app import create_app, db
+from app.models import User
+from werkzeug.security import generate_password_hash
 
 app = create_app()
 
+# Executa somente se o banco ainda estiver vazio
+with app.app_context():
+    db.create_all()
+    if not User.query.first():
+        admin = User(username='admin', password=generate_password_hash('admin123'))
+        db.session.add(admin)
+        db.session.commit()
+        print("Usuário admin criado.")
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
+
