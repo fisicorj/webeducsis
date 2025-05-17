5e6d2d8a5311461a82414daff48bcf5e80b87242
Manoel Guilherme <fisicorj@gmail.com>
2025-05-17 19:00:15 -0300

    Update routes.py

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,26 +1,25 @@
 
-from flask import Blueprint, render_template, redirect, url_for, request, flash
+from flask import Blueprint, render_template, redirect, url_for, flash, request
 from flask_login import login_user, logout_user, login_required
-from werkzeug.security import check_password_hash
+from app.models import Usuario
 from app.forms import LoginForm
-from app.models import User
-from app import db
+from app.extensions import db
 
-auth_bp = Blueprint('auth', __name__)
+auth_bp = Blueprint('auth_bp', __name__, url_prefix='/')
 
-@auth_bp.route('/login', methods=['GET', 'POST'], endpoint='auth_bp.login')
+@auth_bp.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user and check_password_hash(user.password, form.password.data):
+        user = Usuario.query.filter_by(username=form.username.data).first()
+        if user and user.check_password(form.password.data):
             login_user(user)
-            return redirect(url_for('painel.painel'))
-        flash('Usuário ou senha inválidos')
-    return render_template('login.html', form=form)
+            return redirect(url_for('dashboard.index'))
+        flash('Credenciais inválidas.', 'danger')
+    return render_template('auth/login.html', form=form)
 
 @auth_bp.route('/logout')
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('auth.login'))
+    return redirect(url_for('auth_bp.login'))
