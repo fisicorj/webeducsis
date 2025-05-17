ff8a36114b954228b6959770351b771acdf830b2
Manoel Guilherme <fisicorj@gmail.com>
2025-05-17 14:45:47 -0300

    update 2

--- app.py
+++ app.py
@@ -1,38 +1,90 @@
-from flask import Flask, render_template, g
-import sqlite3
+
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+import sqlite3, os
+import pandas as pd
 
 app = Flask(__name__)
-DATABASE = 'controle_turma_manha.db'
+app.secret_key = 'super_secret'
+
+DATABASE = 'controle_turma_manha_v2.db'
 
 def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
-        db.row_factory = sqlite3.Row
-    return db
-
-@app.teardown_appcontext
-def close_connection(exception):
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.close()
+    conn = sqlite3.connect(DATABASE)
+    conn.row_factory = sqlite3.Row
+    return conn
 
 @app.route('/')
 def index():
+    if 'user' not in session:
+        return redirect(url_for('login'))
     db = get_db()
-    cur = db.execute('SELECT * FROM alunos')
-    alunos = cur.fetchall()
+    alunos = db.execute('SELECT * FROM alunos').fetchall()
     return render_template('index.html', alunos=alunos)
 
-@app.route('/aluno/<id_aluno>')
-def aluno(id_aluno):
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        user = request.form['username']
+        pwd = request.form['password']
+        db = get_db()
+        result = db.execute('SELECT * FROM usuarios WHERE username=? AND password=?', (user, pwd)).fetchone()
+        if result:
+            session['user'] = user
+            return redirect(url_for('index'))
+        flash('Credenciais inválidas')
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('login'))
+
+@app.route('/cadastrar-aluno', methods=['GET', 'POST'])
+def cadastrar_aluno():
+    if 'user' not in session:
+        return redirect(url_for('login'))
+    if request.method == 'POST':
+        id_aluno = request.form['id_aluno']
+        nome = request.form['nome']
+        db = get_db()
+        db.execute('INSERT INTO alunos (id_aluno, nome) VALUES (?, ?)', (id_aluno, nome))
+        db.commit()
+        return redirect(url_for('index'))
+    return render_template('cadastrar.html')
+
+@app.route('/importar-alunos', methods=['GET', 'POST'])
+def importar_alunos():
+    if 'user' not in session:
+        return redirect(url_for('login'))
+    if request.method == 'POST':
+        f = request.files['file']
+        if f:
+            df = pd.read_csv(f)
+            db = get_db()
+            for _, row in df.iterrows():
+                db.execute('INSERT OR IGNORE INTO alunos (id_aluno, nome) VALUES (?, ?)', (row['id_aluno'], row['nome']))
+            db.commit()
+            return redirect(url_for('index'))
+    return render_template('importar.html')
+
+@app.route('/editar-notas/<id_aluno>', methods=['GET', 'POST'])
+def editar_notas(id_aluno):
+    if 'user' not in session:
+        return redirect(url_for('login'))
     db = get_db()
-    aluno = db.execute('SELECT * FROM alunos WHERE id_aluno = ?', (id_aluno,)).fetchone()
+    if request.method == 'POST':
+        p1 = request.form['p1']
+        p2 = request.form['p2']
+        lt = request.form['lt']
+        projeto = request.form['projeto']
+        mf = request.form['mf']
+        db.execute('REPLACE INTO notas_finais (id_aluno, p1, p2, lt, projeto, mf) VALUES (?, ?, ?, ?, ?, ?)',
+                   (id_aluno, p1, p2, lt, projeto, mf))
+        db.commit()
+        return redirect(url_for('index'))
     notas = db.execute('SELECT * FROM notas_finais WHERE id_aluno = ?', (id_aluno,)).fetchone()
-    provas = db.execute('SELECT * FROM provas WHERE id_aluno = ?', (id_aluno,)).fetchone()
-    atividades = db.execute('SELECT * FROM atividades WHERE id_aluno = ?', (id_aluno,)).fetchone()
-    projeto = db.execute('SELECT * FROM projetos WHERE id_aluno = ?', (id_aluno,)).fetchone()
-    return render_template('aluno.html', aluno=aluno, notas=notas, provas=provas, atividades=atividades, projeto=projeto)
+    aluno = db.execute('SELECT * FROM alunos WHERE id_aluno = ?', (id_aluno,)).fetchone()
+    return render_template('editar.html', aluno=aluno, notas=notas)
 
 if __name__ == '__main__':
     app.run(debug=True)
