f837e957c5fb6eefb004c8858d7457b37e978ca9
Manoel Guilherme <fisicorj@gmail.com>
2025-05-18 11:05:02 -0300

    up61

--- app/forms.py
+++ app/forms.py
@@ -3,11 +3,11 @@
 class DisciplinaForm(FlaskForm):
     nome = StringField('Nome da Disciplina', validators=[DataRequired()])
     professor = StringField('Professor Responsável')
-    turma_id = SelectField('Turma', coerce=int)
+    turma_id = SelectField('Turma', coerce=int, validators=[DataRequired()])
     submit = SubmitField('Salvar')
 
 
 class MatriculaForm(FlaskForm):
-    aluno_id = SelectField('Aluno', coerce=int)
-    disciplina_id = SelectField('Disciplina', coerce=int)
+    aluno_id = SelectField('Aluno', coerce=int, validators=[DataRequired()])
+    disciplina_id = SelectField('Disciplina', coerce=int, validators=[DataRequired()])
     submit = SubmitField('Matricular')
