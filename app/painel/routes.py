acc3f084b4b73b10f0c55f109c363f3337aa194b
Manoel Guilherme <fisicorj@gmail.com>
2025-05-17 21:02:22 -0300

    Update routes.py

--- app/painel/routes.py
+++ app/painel/routes.py
@@ -34,38 +34,3 @@ def novo():
     return render_template('painel/form.html', form=form, titulo='Novo Painel')
 
 
-@painel_bp.route('/')
-@login_required
-def listar():
-    itens = Painel.query.all()
-    return render_template('painel/listar.html',
-        titulo="Painel",
-        novo_url='painel.novo',
-        editar_url='painel.editar',
-        excluir_url='painel.excluir',
-        cabecalhos=['ID'],
-        campos=['id'],
-        itens=itens)
-
-
-@painel_bp.route('/excluir/<int:id>')
-@login_required
-def excluir(id):
-    obj = Painel.query.get_or_404(id)
-    db.session.delete(obj)
-    db.session.commit()
-    flash('Painel excluído.')
-    return redirect(url_for('painel.listar'))
-
-from app.models import Aluno, Turma, Instituicao
-
-@painel_bp.route('/')
-@login_required
-def painel():
-    total_alunos = Aluno.query.count()
-    total_turmas = Turma.query.count()
-    total_instituicoes = Instituicao.query.count()
-    return render_template('painel/painel.html',
-                           total_alunos=total_alunos,
-                           total_turmas=total_turmas,
-                           total_instituicoes=total_instituicoes)
